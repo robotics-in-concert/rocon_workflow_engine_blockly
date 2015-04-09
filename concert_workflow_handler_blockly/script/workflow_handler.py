6941c734c29abc2faa5ab0059ae182103a5f8e91
dwlee <dwlee@yujinrobot.com>
2015-04-09 18:41:50 +0900

    name fix

--- concert_workflow_handler_blockly/script/workflow_handler.py
+++ concert_workflow_handler_blockly/script/workflow_handler.py
@@ -22,12 +22,12 @@ if __name__ == '__main__':
     sfc = None
     try:
         sfc = SoftwareFarmClient()
-        success, namespace, _unused_parameters = sfc.allocate("concert_workflow_engine/workflow_engine_blockly")
+        success, namespace, _unused_parameters = sfc.allocate("concert_workflow_engine_blockly/workflow_engine_blockly")
         if not success:
             raise FailedToStartSoftwareException("Failed to allocate software")
-        wf_handler = concert_service_workflows.WorkflowHandlerBlockly(service_name, wf, namespace)
+        wf_handler = concert_workflow_handler_blockly.WorkflowHandlerBlockly(service_name, wf, namespace)
         rospy.on_shutdown(wf_handler.shutdown)
-        rospy.on_shutdown(lambda : sfc.deallocate("concert_workflow_engine/workflow_engine_blockly"))
+        rospy.on_shutdown(lambda : sfc.deallocate("concert_workflow_engine_blockly/workflow_engine_blockly"))
         wf_handler.spin()
 
     except (FailedToStartSoftwareException, rospkg.ResourceNotFound) as e:
